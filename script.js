6731c085480145c40743fb895acb4707b6e1e7ee
eloyball <eloyballesteros@hotmail.com>
2022-09-11 17:06:18 -0400

    Cleanup

--- script.js
+++ script.js
@@ -1,40 +1,26 @@
 
-/* 
-   Welcome to the Magic 8-ball Project.  Complete the 
-   tasks outlined by the comments in this file.
+function makeAnswerAppear(answerText) {
+  // Adds "hidden" CSS class to the '8'.
+  document.getElementById("eight").classList.add("hidden");
 
-   The following functions are available to you 
-   (you can just call them):
+  // Find the answer element, change the text, and remove the CSS "hidden" class.
+  document.getElementById("answer-text").innerText = answerText;
 
-   // This will hide the '8' (if shown) and 
-   // show the triangle (if hidden).  It will
-   // set the text on the triangle to the
-   // string 'answerText'.
-   function makeAnswerAppear(answerText)
-*/
+  document.getElementById("answer-text").classList.add("hidden");
+  document.getElementById("triangle").classList.add("hidden");
 
-/* Returns a random integer in the range 'min' through 'max' inclusive. 
+  // Allow 1/1000th of a second to pass in order for the above changes to take effect.  Then remove the elements
+  setTimeout(() => {
+    document.getElementById("answer-text").classList.remove("hidden");
+    document.getElementById("triangle").classList.remove("hidden");
+  }, 1);
+}
 
-   This can be taken directly from MDN documentation: 
-     https://tinyurl.com/3jkxa8h3
-
-*/
 function getRandomIntInclusive(min, max) {
   let randomNum = Math.floor(Math.random() * (max - min + 1) + min);
   return randomNum;
 }
 
-/* Write a function that handles the magic 8-ball being clicked. Here are the steps:
-
-   - Create an array containing at least 5 possible answers as strings.
-   - Generate a random array index by calling 
-       getRandomIntInclusive function.
-   - Call 'makeAnswerAppear' using the random
-       answer you selected.
-   - (Level-up) Prevent your code from selecting the 
-       same answer multiple times in a row 
-       (loops could be required).
-*/
 let previousAnswer = "Hello World!";
 
 function answerQuestion() {
@@ -50,13 +36,11 @@ function answerQuestion() {
 
   makeAnswerAppear(currentAnswer);
 
-  // Fun stuff ;)
   stopTimer();
   startTimer();
 }
 
-// Extra code I added for fun. It basically checks if the input has something the user wrote on it before running the answerQuestion function. No, I did not look this up, I did it myself and yes, I'm super proud :)
-
+// Check if user has made a question
 const clickable = document.getElementById("window");
 
 clickable.addEventListener('click', () => {
@@ -68,8 +52,7 @@ clickable.addEventListener('click', () => {
   }
 });
 
-// Also added this timeout function for fun which turns shows the 8 again, by reverse engineering the provided function, if the user doesn't click on the ball again in 10s.
-
+// Allow magic ball timeout
 let answerTimeout;
 
 function startTimer() {
